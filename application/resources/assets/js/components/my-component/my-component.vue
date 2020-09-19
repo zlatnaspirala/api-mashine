e0a89ac35ebac017e045e8777a253ab7cd1a0fde
Nidza <zlatnaspirala@gmail.com>
2020-09-19 02:12:23 +0200

    Update my-component.vue

--- application/resources/assets/js/components/my-component/my-component.vue
+++ application/resources/assets/js/components/my-component/my-component.vue
@@ -56,7 +56,7 @@ export default class myComponent extends Vue {
 
   prepareComponent() {
     console.log("My component loaded...")
-    this.testRun()
+    // this.testRun()
   }
 
   testRun () {
@@ -66,13 +66,13 @@ export default class myComponent extends Vue {
 
       console.log("test run")
 
-      fetch('/api/v1/posts/51', {
+      fetch('/api/v1/posts/1', {
           method: 'GET',
           headers: {
               'Content-Type': '*',
               'Accept': 'application/json, text/plain, */*',
               'X-CSRF-TOKEN': document.cookie.split("TOKEN=")[1],
-              // 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImMyMzgzN2JkMWZlOTM4YmFlM2E5ODMwZTM2N2Q2ZTM0YWU4NTQyYzQwOGM0ODdiYTNmNjkxNzlhNDc0NTQxMWZlZGM3YmQwZThhZTYwMTg1In0.eyJhdWQiOiIxIiwianRpIjoiYzIzODM3YmQxZmU5MzhiYWUzYTk4MzBlMzY3ZDZlMzRhZTg1NDJjNDA4YzQ4N2JhM2Y2OTE3OWE0NzQ1NDExZmVkYzdiZDBlOGFlNjAxODUiLCJpYXQiOjE2MDAyNzcwODksIm5iZiI6MTYwMDI3NzA4OSwiZXhwIjoxNjMxODEzMDg5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.Uh7cr3dBsIh5_G2vrNfzjFq5efSdMZ3Hc6hjUobZo6VLTUuA_ihCZjADU6BKD-Ht_T67aJ0Afm94IJeHFy7yCHec8mQaJaQm4qW2Mz88xkEIRvNZR7gXbB86mtonzWJGKiMb9yabejGZDiIEBrF7OKJKmpMslzU0SnCNRUcAEs-GJy4UYNG0S7U_OaUy2hhDBVYBCQd2qKpdjjRmH6BKBmjipj9PtM7pa654-GGKDDJ0j-EJI96740vNcy8seLsrcjZmUT6gix_znVyglta4d8tT4S9bj2VTmBMZ6g6DlWf83NjewhzXDwiXBlNAAx39RcYxhK6BK7XXzLgYddKC6HDYsJ1syt-c5hLK4oCsPmKc71FBnQ6x70G8J3L9yn3lzK80q4Y6QBl5pXAZIYtPtdYtak9e3kwGMIg5w8zVz3JdqF9-tbLkWIOags6KTz4ZJb7RP7ShZCyKQZYvMKldNYibOzYgnGVQM9eoA5ULaa8bjepPmqEz5kaAJC248IsezfJUYSaJDh1DoduzM3j7r4mjtviCJ910j-3Om-Oo1DAEsYUnJKKLtCsLIGzlTXK7M392Kg3Va5rargOjOEYWl4eHo1Q7D4GX-gfWxBsGh2Q1vfmjWMKaoT65zuWz_wl4n58dw9cmXyTiD7YZmH_gsTLn_1Oza4CSMhsnhLZHjVk'
+              // 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ1MzhmMzdiYmE1NDU4NTA0YWY0NzE3MWJlYjlkZTBjNjYyYjlmZTM5ZmNiZTU5M2I5M2VjNjgxNGI4ODkwNWQzZjE3NmRmMDk5Y2M4Yzg3In0.eyJhdWQiOiIyIiwianRpIjoiZDUzOGYzN2JiYTU0NTg1MDRhZjQ3MTcxYmViOWRlMGM2NjJiOWZlMzlmY2JlNTkzYjkzZWM2ODE0Yjg4OTA1ZDNmMTc2ZGYwOTljYzhjODciLCJpYXQiOjE2MDA0NzQyMTUsIm5iZiI6MTYwMDQ3NDIxNSwiZXhwIjoxNjMyMDEwMjE1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.HId8yWDDEqXYo48SuK-y2sXnFktpCz-5i9vgchtlFa7rwBcoVgNRBg0fOEw0nqQz2PAtMXBoihJqhjuAaRrFh76LyT_RnPZegIETrFn7YQSDYcwTeLFsTA5iReESt0Z2sNKSzaUFOTNekumSOHuqkx9tftl4FF6TFRtGJ9Z7FSFEi2dbC6pyjngin0Y02DyrzzTF6guvtwosU4CvlXmyG6UKOFeke4SWZ_Qikcmuu5ygy62feIMNqyOr0NW_88JVqtwQNl2fLwJaZ1FaRVxxzR4R3rZNGFhDkN7AOnudD2MYIOzZV7as-A6a1zkrIHoohhS14bvZRfpp-OB_S-go9bUBlceBIrBsia4RhCXwvNCEDAmvyNS3QJT12N_KhDH1emEyMHK2ZcREcr6nLuj21ce4dQkCBepY4T4ysVOIcxgi0rInTrbBXRpLgZ0PHPolM7RLivKmWTJw3oopfmcaOY7f6vLAZaEfoBBUpo2asJ5JxEGYHHj_5aJWhNHSTxqPlv55yTsAjz0apJ_yfGGczJ7PRfOW-c-h3BQxNGwTV-H1grbtYM7jeb2HCa0tzqzu6V05Y7AbinL59d1Sd23HU3k5YCLZdMHR6cU5wyWxDEbHyvl7gZu9iinSuQrKWmqz9avyrJhVIlVWlsqSynCZDc--DjqSzvS-5cJ3p6yHC00'
           },
           // body: JSON.stringify(root.data.data) // body data type must match "Content-Type" header
         })
@@ -106,7 +106,7 @@ export default class myComponent extends Vue {
             'Accept': '*/*',
              // 'Content-Type': 'application/vnd.api+json',
              // 'Accept': 'application/vnd.api+json',
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImFhMGE5NDc3ZTA0OGY2ZWRkYjk5ZTFkNTBkMzE4NTA5ZjU3OTVlYTE5YTFkNzFiZjNhMzJkYzE0YjJiZjljNTM3YTk1ZWRiM2FjNDY5ZDI5In0.eyJhdWQiOiIxIiwianRpIjoiYWEwYTk0NzdlMDQ4ZjZlZGRiOTllMWQ1MGQzMTg1MDlmNTc5NWVhMTlhMWQ3MWJmM2EzMmRjMTRiMmJmOWM1MzdhOTVlZGIzYWM0NjlkMjkiLCJpYXQiOjE2MDA0NDIxNjEsIm5iZiI6MTYwMDQ0MjE2MSwiZXhwIjoxNjMxOTc4MTYxLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.hR7O4URXwJ80AmQwdHqrNNuFlqYeWfab_mIqMm7b4yFt8neofklQizuW6FsRe8N1K3kHB59l8MJIj6LbnWv_N928Fmsb2-C2_G5cbJcRI2WdEzNEHNC-4MmtY05lQEeTQ3KyxUh0gF-uFyLfkSbr_0WiMaXSp1tmYDo6v0GEeYXl3jOUYcdNaflBmpWQF3jVMGWShPFWKxG-W1sAIHv4o3s3nIoUEDWdrL1ig_OZV9msU07S34tmRmcLeKRqrfgUpqwmvqUjYzT20Yk2vAqmuGzlelA5JOfbF--NARJmYGDNjkwtn-7O56LOjVIHJGabTuTBm_-UGWMQSHfyyW5PsW0N0HMGWk_wwOMDed1D3KtB9qfCJOjXvAgCtZDgTwjgG1duP2rTvn4QrZ-5k2N0fsQC89ddc0RAP7kXC3l3Z-w6Wtn28VbsP-CBLTHXB1DTDw1cuk0eB4C-oR9fwSkPugyp_vF3IzUNpHOSsbBVFeUPnBj_Vq8V492DlOLMZRZiEDZREI4yyNiBpKjHSksL3rVzi08lamdsnZtkkwHacTq4KJGWG5tTpHu0mFhwZ4olpQLuJqJzQXHS1TGhIqbyeuLwJqwSklXmxXRu-Zm5sJPpeYCjy3vGi07AtyLx3izflQxeAIMFbxmBeppAVc3R1Jpl-5RnM1XZB3UBsrJu9S8',
+             'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ1MzhmMzdiYmE1NDU4NTA0YWY0NzE3MWJlYjlkZTBjNjYyYjlmZTM5ZmNiZTU5M2I5M2VjNjgxNGI4ODkwNWQzZjE3NmRmMDk5Y2M4Yzg3In0.eyJhdWQiOiIyIiwianRpIjoiZDUzOGYzN2JiYTU0NTg1MDRhZjQ3MTcxYmViOWRlMGM2NjJiOWZlMzlmY2JlNTkzYjkzZWM2ODE0Yjg4OTA1ZDNmMTc2ZGYwOTljYzhjODciLCJpYXQiOjE2MDA0NzQyMTUsIm5iZiI6MTYwMDQ3NDIxNSwiZXhwIjoxNjMyMDEwMjE1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.HId8yWDDEqXYo48SuK-y2sXnFktpCz-5i9vgchtlFa7rwBcoVgNRBg0fOEw0nqQz2PAtMXBoihJqhjuAaRrFh76LyT_RnPZegIETrFn7YQSDYcwTeLFsTA5iReESt0Z2sNKSzaUFOTNekumSOHuqkx9tftl4FF6TFRtGJ9Z7FSFEi2dbC6pyjngin0Y02DyrzzTF6guvtwosU4CvlXmyG6UKOFeke4SWZ_Qikcmuu5ygy62feIMNqyOr0NW_88JVqtwQNl2fLwJaZ1FaRVxxzR4R3rZNGFhDkN7AOnudD2MYIOzZV7as-A6a1zkrIHoohhS14bvZRfpp-OB_S-go9bUBlceBIrBsia4RhCXwvNCEDAmvyNS3QJT12N_KhDH1emEyMHK2ZcREcr6nLuj21ce4dQkCBepY4T4ysVOIcxgi0rInTrbBXRpLgZ0PHPolM7RLivKmWTJw3oopfmcaOY7f6vLAZaEfoBBUpo2asJ5JxEGYHHj_5aJWhNHSTxqPlv55yTsAjz0apJ_yfGGczJ7PRfOW-c-h3BQxNGwTV-H1grbtYM7jeb2HCa0tzqzu6V05Y7AbinL59d1Sd23HU3k5YCLZdMHR6cU5wyWxDEbHyvl7gZu9iinSuQrKWmqz9avyrJhVIlVWlsqSynCZDc--DjqSzvS-5cJ3p6yHC00',
             'X-CSRF-TOKEN': document.cookie.split("TOKEN=")[1]
         },
         body: JSON.stringify(mydata)
